c292700a8726eba7b51d132ac8b3204e63558b99
Steven Jixiao Wang <wangjx@shinetechchina.com>
2018-01-26 11:30:52 +0800

    添加react-router-native

--- App.js
+++ App.js
@@ -3,19 +3,36 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import { Provider, connect } from 'react-redux';
 import ConfigStore from './src/stores/index';
+import { NativeRouter, Route, Link } from 'react-router-native'
 
 import UserContainer from "./src/container/user-container";
 
 const store = ConfigStore();
 
+const Default = () => (
+  <Text>
+    Default View
+  </Text>
+)
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <View style={styles.container}>
-        <Text>Login Request</Text>
-          <UserContainer/>
-        </View>
+        <NativeRouter>
+          <View style={styles.container}>
+            <View>
+              <Link to = "/"><Text>Default</Text></Link>
+              <Link to="/u"><Text>User Container</Text></Link>
+            </View>
+            
+            <Switch>
+              <Route exact path="/" component={Default}/>
+              <Route path ="/u" component={UserContainer}/>
+            </Switch>
+          </View>
+        </NativeRouter>
+
       </Provider>
     );
   }
